a637266ca9a6ef7a3cd6f01e6a9ff386d68818e0
Olleb209 <bragsjoolle@gmail.com>
2026-03-01 20:23:13 +0100

    tuning

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 800.0
+const SPEED = 3600.0
 const JUMP_VELOCITY = -400.0
 
 func lerp_vec2(a: Vector2, b: Vector2, t: float) -> Vector2:
@@ -10,7 +10,7 @@ func _physics_process(delta: float) -> void:
 	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized()
 	if direction:
 		velocity += direction * SPEED * delta
-	
-	velocity = lerp_vec2(velocity, Vector2(0, 0), 5.0*delta)
+
+	velocity = lerp_vec2(velocity, Vector2(0, 0), 7.5*delta)
 
 	move_and_slide()
